22b39fb1cb4b22e42fdb53a45ffe7b2d9d5504d7
jiahuiz1 <jiahuiz@ksu.edu>
2022-03-15 19:27:15 -0700

    Movie List page
    Finish the basic design of movie list page

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MovieListPage from './containers/MovieListPage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Bootstrap</Button>
-      </header>
+      <MovieListPage/>
     </div>
   );
 }
---
+++ movie-list/src/containers/MovieListPage.js
@@ -0,0 +1,62 @@
+import {Button, Spinner} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MovieListPage.css';
+import Movie from '../components/Movie';
+import {useState, useEffect} from 'react';
+
+function MovieListPage(){
+    const [data, setData] = useState([]);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
+    const [totalPage, setTotalPage] = useState(0);
+
+    useEffect(()=>{
+        fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0f207dd93db6d0bd617a7ee5e6e6edee&page=${pageNumber}&append_to_response=production_companies`)
+        .then((result) => {
+            return result.json();
+        })
+        .then((d)=>{
+            //console.log(d);
+            const temp = d.results;
+            setData([...data, ...temp]);
+            setPageNumber(d.page);
+            setTotalResults(d.total_results);
+            setTotalPage(d.total_pages);
+        })
+    }, []);
+
+    console.log(data);
+    return(
+        <div className="MovieListPage">
+            <header>
+                <h1>The Most Popular Movies</h1>
+            </header>
+            <nav><p>navigation bar</p></nav>
+            <h2>Movies List Page</h2>
+            <main>
+                <div className="sortButtons">
+                    <Button size="lg">Title</Button>
+                    <Button size="lg">Vote Count</Button>
+                    <Button size="lg">Vote Average</Button>
+                    <Button size="lg">Release Date</Button>
+                </div>
+                {data.length===0 && <Spinner animation="border" variant="primary"/>}
+                <div className="pageNav">
+                    <Button variant="dark">Prev</Button>
+                    <div className="pageInfo">Page {pageNumber}/Total {totalPage} pages of {totalResults} results</div>
+                    <Button variant="dark">Next</Button>
+                </div>
+                <div className="movieBlock">
+                    {data.length > 0 && data.map((item) => {
+                        return (
+                            <Movie item={item}/>
+                        )
+                    })}
+                </div>
+            </main>
+
+        </div>
+    )
+}
+
+export default MovieListPage;
